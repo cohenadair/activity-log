e126101f0f8ee9853f91128bb93499495fc9790b
cohenadair <cohenadair@gmail.com>
2019-01-23 16:47:24 -0500

    lib/sql_data_manager -> lib/database/sqlite_data_manager

--- mobile/lib/app_manager.dart
+++ mobile/lib/app_manager.dart
@@ -1,5 +1,5 @@
 import 'package:mobile/data_manageable.dart';
-import 'package:mobile/sql_data_manager.dart';
+import 'package:mobile/database/sqlite_data_manager.dart';
 
 class AppManager {
   DataManageable _dataManager;
