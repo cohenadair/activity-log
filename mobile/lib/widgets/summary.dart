2d2ab93a361cf86056253ae044cf32a6f3cf7883
cohenadair <cohenadair@gmail.com>
2019-03-16 19:20:58 -0400

    Updated Summary to use a ListItem

--- mobile/lib/widgets/summary.dart
+++ mobile/lib/widgets/summary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/res/dimen.dart';
+import 'package:mobile/widgets/list_item.dart';
 import 'package:mobile/widgets/text.dart';
 import 'package:mobile/widgets/widget.dart';
 import 'package:quiver/strings.dart';
@@ -33,33 +34,14 @@ class Summary extends StatelessWidget {
 
   List<Widget> _buildItems(BuildContext context) {
     return items.map((SummaryItem item) {
-      return Padding(
-        padding: insetsRowDefault,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  item.title,
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-                Text(
-                  item.value is String ? item.value : item.value.toString(),
-                  style: Theme.of(context).textTheme.subhead.copyWith(
-                    color: Theme.of(context).disabledColor,
-                  ),
-                ),
-              ],
-            ),
-            isEmpty(item.subtitle) ? Empty() : Text(
-              item.subtitle,
-              style: Theme.of(context).textTheme.subtitle.copyWith(
-                color: Theme.of(context).disabledColor,
-              ),
-            ),
-          ],
+      return ListItem(
+        title: Text(item.title),
+        subtitle: isEmpty(item.subtitle) ? null : Text(item.subtitle),
+        trailing: Text(
+          item.value is String ? item.value : item.value.toString(),
+          style: Theme.of(context).textTheme.subhead.copyWith(
+            color: Theme.of(context).disabledColor,
+          ),
         ),
       );
     }).toList();
