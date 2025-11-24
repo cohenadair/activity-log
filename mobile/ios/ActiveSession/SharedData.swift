79b996d67c7b2e5aaa4c7143c7ac00b7abffd11b
Cohen Adair <cohenadair@gmail.com>
2025-11-24 09:06:41 -0500

    Check for existing ID before adding to shared data on iOS

--- mobile/ios/ActiveSession/SharedData.swift
+++ mobile/ios/ActiveSession/SharedData.swift
@@ -11,13 +11,18 @@ import WidgetKit
 // Seems to be required at a global scope for the live activity to show up.
 // Using a singleton didn't work as I expected it would.
 let defaults = UserDefaults(suiteName: "group.cohenadair.activitylog")!
+let keyEndedActivityIds = "ended_activity_ids"
 
 func appendLog(_ log: String) {
     appendStringArray("logs", log)
 }
 
 func appendEndedActivity(_ activityId: String) {
-    appendStringArray("ended_activity_ids", "\(activityId):\(Int(Date().timeIntervalSince1970 * 1000))")
+    if (defaults.stringArray(forKey: keyEndedActivityIds) ?? []).contains(where: { $0.starts(with: activityId) }) {
+        appendLog("Ended activity (\(activityId)) already exists in shared data")
+        return
+    }
+    appendStringArray(keyEndedActivityIds, "\(activityId):\(Int(Date().timeIntervalSince1970 * 1000))")
 }
 
 private func appendStringArray(_ key: String, _ value: String) {
